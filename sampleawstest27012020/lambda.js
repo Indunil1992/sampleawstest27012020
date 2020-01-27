d1ad1082370e47146253721eb2df6e1a67c429d9
Indunil1992 <indunil@adroitlogic.com>
2020-01-27 11:23:36 +0530

    2

--- sampleawstest27012020/lambda.js
+++ sampleawstest27012020/lambda.js
@@ -3,9 +3,9 @@ const translate = new AWS.Translate();
 
 exports.handler = async (event) => {
     translate.translateText({
-        SourceLanguageCode: "tr",
-        TargetLanguageCode: "en",
-        Text: "baba"
+        SourceLanguageCode: "trw",
+        TargetLanguageCode: "enw",
+        Text: "babaw"
     }).promise()
         .then(data => {
             // your code goes here
