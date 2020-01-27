6e7d4b4befccc7bb42ee9a8adf11d43487beaa10
Indunil1992 <indunil@adroitlogic.com>
2020-01-27 11:18:22 +0530

    G translate with turky

--- sampleawstest27012020/lambda.js
+++ sampleawstest27012020/lambda.js
@@ -3,14 +3,16 @@ const translate = new AWS.Translate();
 
 exports.handler = async (event) => {
     translate.translateText({
-        SourceLanguageCode: "it",
+        SourceLanguageCode: "tr",
         TargetLanguageCode: "en",
-        Text: "pita"
+        Text: "baba"
     }).promise()
         .then(data => {
             // your code goes here
+            console.log(data);
         })
         .catch(err => {
+            console.log(err);
             // error handling goes here
         });
 
